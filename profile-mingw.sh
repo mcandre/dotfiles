f40000287a8baca783c25cf33a0634a992060a60
mcandre <andrew.pennebaker@gmail.com>
2013-04-04 10:07:06 -0400

    Git Bash

--- profile-mingw.sh
+++ profile-mingw.sh
@@ -1,2 +1,25 @@
-export PS1="\w$ "
-cd /c/Users/andrew/Desktop
+# Monokai
+D=$'\e[37;40m'
+PINK=$'\e[35;40m'
+
+BOLD=$'\033[1m'
+
+export PS1='${bold}${PINK}\u:\w\$${D}${normal} '
+export CLICOLOR=1
+
+# Specs
+export PATH=$PATH:~/Desktop/src/specs
+
+# Sublime Text
+export PATH=$PATH:"C:\Program Files\Sublime Text 2"
+
+# xgit
+export PATH=$PATH:"/c/Documents and Settings/apenneba/Desktop/src/github-teams"
+
+# Chocolatey
+export PATH=$PATH:"/c/Chcolatey/bin"
+
+# NIC+
+export PATH=$PATH:"/c/Documents and Settings/apenneba/Desktop/src/nicplus/node_modules/.bin"
+
+cd ~/Desktop
