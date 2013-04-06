46e16d6a38fd8f2bea4a6d8cdcf40b2c81663848
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-04-06 00:07:57 -0400

    fixed Emacs opening in the background

--- emacs.sh
+++ emacs.sh
@@ -6,4 +6,5 @@ if [[ $@ =~ -nw ]]; then
   $EMACS_BIN ${1+"$@"};
 else
   nohup $EMACS_BIN ${1+"$@"} > /dev/null &
+  osascript -e 'tell application "System Events" to set frontmost of process "Emacs" to true'
 fi
