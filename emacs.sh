7e2b9cb2b0da9e43d32dd536e5e6219a4c515267
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2013-04-06 00:19:15 -0400

    improved ncurses detection

--- emacs.sh
+++ emacs.sh
@@ -4,7 +4,7 @@
 
 EMACS_BIN=/Applications/Emacs.app/Contents/MacOS/Emacs
 
-if [[ $@ =~ -nw ]]; then
+if [[ $@ =~ -nw|--help|--version ]]; then
   $EMACS_BIN ${1+"$@"};
 else
   nohup $EMACS_BIN ${1+"$@"} > /dev/null &
