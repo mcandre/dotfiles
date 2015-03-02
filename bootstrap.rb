e469422594eddf3d3efdf4fd0635ec7c3b504677
Andrew Pennebaker <andrew.pennebaker@networkedinsights.com>
2015-03-02 16:32:28 -0600

    editorconfig

--- bootstrap.rb
+++ bootstrap.rb
@@ -9,5 +9,7 @@ mods = ARGV
 installed = `puppet module list`.split "\n"
 
 mods.each do |mod|
-  puts `puppet module install #{mod}` unless installed.any? { |i| i.include?(mod.sub('/', '-')) }
+  puts `puppet module install #{mod}` unless installed.any? do |i|
+    i.include?(mod.sub('/', '-'))
+  end
 end
