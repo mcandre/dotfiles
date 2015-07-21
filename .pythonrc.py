666173793b4fcec12d165b86553fe41ca7a2a3d7
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-07-21 09:30:05 -0500

    python repl history and tab completion

---
+++ .pythonrc.py
@@ -0,0 +1,9 @@
+# ln -s .../.pythonrc.py ~/.pythonrc.py
+
+try:
+    import readline
+except ImportError:
+    print("Module readline not available.")
+else:
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
