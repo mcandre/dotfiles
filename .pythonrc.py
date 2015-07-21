c5dba85674bae58950f3abd95a08c4f24c35cac6
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-07-21 09:54:01 -0500

    persist python history across sessions

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,9 +1,28 @@
+# ln -s .../python.sh ~/.bashrc.d/enabled/python.sh
 # ln -s .../.pythonrc.py ~/.pythonrc.py
 
-try:
+# Add tab completion and session history to REPL
+
+import readline
+import rlcompleter
+
+#
+# Persist history across sessions
+#
+
+import atexit
+import os
+
+history_path = os.path.expanduser("~/.pyhistory")
+
+def save_history(history_path = history_path):
     import readline
-except ImportError:
-    print("Module readline not available.")
-else:
-    import rlcompleter
-    readline.parse_and_bind("tab: complete")
+    readline.write_history_file(history_path)
+
+if os.path.exists(history_path):
+    readline.read_history_file(history_path)
+
+atexit.register(save_history)
+
+# Clear variables from REPL
+del os, atexit, readline, rlcompleter, save_history, history_path
