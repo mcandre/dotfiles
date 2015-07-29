45eb25cd5abd8843c0737c11916ba67cb8f055da
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-07-29 15:55:57 -0500

    pep8

--- .pythonrc.py
+++ .pythonrc.py
@@ -1,22 +1,22 @@
+#
+# Add tab completion and session history to REPL
+#
 # ln -s .../python.sh ~/.bashrc.d/enabled/python.sh
 # ln -s .../.pythonrc.py ~/.pythonrc.py
 
-# Add tab completion and session history to REPL
-
 import readline
 import rlcompleter
-readline.parse_and_bind("tab: complete")
+import atexit
+import os
 
 #
 # Persist history across sessions
 #
 
-import atexit
-import os
-
 history_path = os.path.expanduser("~/.pyhistory")
 
-def save_history(history_path = history_path):
+
+def save_history(history_path=history_path):
     import readline
     readline.write_history_file(history_path)
 
@@ -24,6 +24,8 @@ if os.path.exists(history_path):
     readline.read_history_file(history_path)
 
 atexit.register(save_history)
+
+readline.parse_and_bind("tab: complete")
 
 # Clear variables from REPL
 del os, atexit, readline, rlcompleter, save_history, history_path
--- tasks.py
+++ tasks.py
@@ -1,17 +1,21 @@
 from invoke import run, task
 
+
 @task
 def pep8():
-  run("pep8 .")
+    run("pep8 .")
+
 
 @task
 def pylint():
-  run("pylint *.py")
+    run("pylint *.py")
+
 
 @task
 def pyflakes():
-  run("pyflakes .")
+    run("pyflakes .")
+
 
 @task(pre=[pep8, pylint, pyflakes])
 def lint():
-  pass
+    pass
