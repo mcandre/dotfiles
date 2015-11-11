ff7e1e94a52da5e79edf2149f4e7643ef20841b9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2015-11-10 22:15:53 -0600

    workaround pyenv shims vs brew doctor

--- .bash_profile.d/aliases.sh
+++ .bash_profile.d/aliases.sh
@@ -4,6 +4,9 @@ alias sudo='sudo '
 # Show details
 alias ll='ls -Ahl'
 
+# Workaround pyenv shims vs. brew doctor
+alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
+
 # Colorize soft links
 alias ls='gls --color=auto'
 
