b3ba9e60c9a586a43a3e7890d71490e158693c6c
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-01-15 21:27:01 -0600

    fix brew vs cabal updated cabal

--- .bashrc.d/brew.sh
+++ .bashrc.d/brew.sh
@@ -1,6 +1,5 @@
 PATH="/usr/local/bin:/usr/local/sbin:$PATH"
 PATH="$HOME/.cask/bin:$PATH"
-export PATH
 
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   . $(brew --prefix)/etc/bash_completion
@@ -8,3 +7,6 @@ fi
 
 # Workaround pyenv shims vs. brew doctor
 alias brew="env PATH=${PATH//$(pyenv root)\/shims:/} brew"
+
+# Workaround brew updated cabal vs cabal updated cabal
+export PATH="$HOME/.cabal/bin:$PATH"
