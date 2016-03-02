82192cebbebde193cb301b76487593efa0983a10
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2016-03-02 14:16:35 -0600

    make git prompt safe for vim

--- .bashrc.d/editor-safe-git-prompt.sh
+++ .bashrc.d/editor-safe-git-prompt.sh
@@ -1,4 +1,4 @@
-if [ "$INSIDE_EMACS" ]; then
+if [ "$INSIDE_EMACS" -o "$VIMRUNTIME" ]; then
   PROMPT_COMMAND='' && export PS1='$ '
 else
   . "$HOME/git-prompt.sh" && export PS1='\W$(__git_ps1 " (%s)")$ '
