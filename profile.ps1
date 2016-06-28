4cd660733eccd3a1959672dba90a018edbe6bb84
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-06-27 22:10:10 -0500

    clarify powershell setup steps

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,7 @@
 # Copy to Documents\WindowsPowerShell\profile.ps1
+#
+# Requires elevated permissions to be loaded:
+#
+# PS admin> Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 
 New-Alias vi vim
