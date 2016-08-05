869388e1688fc7d3e3b0143b90e5fcc33b074da3
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-08-04 20:07:56 -0500

    fix powershell alias syntax

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,11 @@
-# Copy to Documents\WindowsPowerShell\profile.ps1
+# Copy or Soft Link to ...\Documents\WindowsPowerShell\profile.ps1
 #
 # Requires elevated permissions to be loaded:
 #
 # PS admin> Set-ExecutionPolicy Unrestricted -Scope CurrentUser
 
-New-Alias vi vim
-New-Alias emacs emacs -nw
+function vi { vim.exe $args }
+function view { vim.exe -R $args }
+function emacs { emacs.exe -nw $args }
 
 & "C:\Program Files\Docker Toolbox\docker-machine.exe" env default | Invoke-Expression
