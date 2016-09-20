18edc23a75ddf2f201c5bb38efae990675d2c159
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-09-20 17:15:20 -0500

    found recursive directory size command in powershell syntax

--- profile.ps1
+++ profile.ps1
@@ -9,5 +9,6 @@ function view { vim.exe -R $args }
 function emacs { emacs.exe -nw $args }
 function ll { cmd /c dir $args }
 function gitignore { curl.exe -k https://www.gitignore.io/api/$args }
+function lsdu { ls $args | select Name,@{n="Length"; e={[long](ls $_ -file -rec | measure Length -sum | % Sum)}} | sort Length -d }
 
 & "C:\Program Files\Docker Toolbox\docker-machine.exe" env default | Invoke-Expression
