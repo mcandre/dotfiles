8d059fdcc3e4a7dfe0e0adcfcfb1bc3a2c6ed551
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2016-10-05 12:52:13 -0500

    lsdu: show non-folder files as well

--- profile.ps1
+++ profile.ps1
@@ -9,6 +9,12 @@ function view { vim.exe -R $args }
 function emacs { emacs.exe -nw $args }
 function ll { cmd /c dir $args }
 function gitignore { curl.exe -k https://www.gitignore.io/api/$args }
-function lsdu { ls $args | select Name,@{n="Length"; e={[long](ls $_ -file -rec | measure Length -sum | % Sum)}} | sort Length -d }
+function lsdu {
+  $folders = Get-ChildItem $args | select Name,@{n="Length"; e={[long](ls $_ -file -rec | measure Length -sum | % Sum)}} | sort Length -d
+  $files = Get-ChildItem -File | select Name, Length | sort Length -d
+  $list = $folders + $files
+
+  return $list
+}
 
 & "C:\Program Files\Docker Toolbox\docker-machine.exe" env default | Invoke-Expression
