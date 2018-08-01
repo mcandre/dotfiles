8be2b94d1bcde42a56019f4bc930e87905d14759
Andrew Pennebaker <andrew.pennebaker@kohls.com>
2018-08-01 07:29:03 -0500

    document git-prompt dependencies

--- .bashrc.d/git-prompt.sh
+++ .bashrc.d/git-prompt.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+#
+# This loader depends on
+# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 . "$HOME/git-prompt.sh" && export PS1='\W$(__git_ps1 " (%s)")$ '
