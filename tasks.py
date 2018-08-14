37aed3b42985d8ff1a2c57a866c535ae56693089
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2018-08-13 22:03:57 -0500

    update yamllint

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+import fnmatch
+import os
 from invoke import run, task
 
 
@@ -21,6 +23,22 @@ def flake8():
     run('flake8 .')
 
 
-@task(pre=[pep8, pylint, pyflakes, flake8])
+@task
+def yamllint():
+    run('yamllint .yamllint')
+
+    matches = []
+    for root, dirname, filenames in os.walk("."):
+        for filename in fnmatch.filter(filenames, "*.yml"):
+            matches.append(os.path.join(root, filename))
+
+        for filename in fnmatch.filter(filenames, "*.yaml"):
+            matches.append(os.path.join(root, filename))
+
+    for m in matches:
+        run('yamllint %s' % m)
+
+
+@task(pre=[pep8, pylint, pyflakes, flake8, yamllint])
 def lint():
     pass
