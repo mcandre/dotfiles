f624042b1fb092f4bce14d2db1044649096efb13
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-09-12 11:55:11 -0500

    shellcheck

--- .profile.d/rsync.sh
+++ .profile.d/rsync.sh
@@ -3,8 +3,9 @@
 DISABLE_IPV6=''
 
 if [ -r /proc/version ] && grep 'Microsoft' /proc/version >/dev/null; then
+    # shellcheck disable=SC2034
     DISABLE_IPV6='-4 '
 fi
 
 # Recursive, 1MB/sec throttle, aggregate progress
-alias rsync="rsync ${DISABLE_IPV6}-a --bwlimit 1000 --info progress2"
+alias rsync='rsync ${DISABLE_IPV6}-a --bwlimit 1000 --info progress2'
