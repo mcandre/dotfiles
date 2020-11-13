0161fe78a0533e754088db56e76bd50817b364d9
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2020-11-13 17:18:31 -0600

    spacing

--- .profile.d/rez.sh
+++ .profile.d/rez.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 rez() {
-    ffprobe -v quiet -print_format json -show_format -show_streams "$1" |
-        jq '.streams[0].height'
+    ffprobe -v quiet \
+        -print_format json \
+        -show_format \
+        -show_streams "$1" |
+            jq '.streams[0].height'
 }
