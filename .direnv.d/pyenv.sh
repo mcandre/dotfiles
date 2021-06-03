c9aca2a5fc996969814afc45fb7f4be216905ad8
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-06-03 10:05:56 -0500

    update pyenv

--- .direnv.d/pyenv.sh
+++ .direnv.d/pyenv.sh
@@ -2,7 +2,7 @@
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-eval "$(pyenv init -)"
+eval "$(pyenv init --path)"
 
 # Fix Ansible
 export ANSIBLE_PYTHON_INTERPRETER="$(which python)"
