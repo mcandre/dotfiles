97d2856500f70f64d0378de30ba061d706caa5ba
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2021-09-19 15:01:33 -0500

    fix vagrant wsl

--- .profile.d/vagrant.sh
+++ .profile.d/vagrant.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ -r /proc/version ] && ! grep -i 'microsoft' /proc/version >/dev/null; then
+if [ -r /proc/version ] && grep -i 'microsoft' /proc/version >/dev/null; then
     export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
 fi
