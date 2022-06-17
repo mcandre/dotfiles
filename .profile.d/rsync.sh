d40e53a52cc0f8c87425dabfe5bbd023f83ce199
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2022-06-17 16:02:23 -0500

    fix rsync on ubuntu

--- .profile.d/rsync.sh
+++ .profile.d/rsync.sh
@@ -7,5 +7,17 @@ if [ -r /proc/version ] && grep 'Microsoft' /proc/version >/dev/null; then
     DISABLE_IPV6='-4'
 fi
 
+DISABLE_NEW_ARGS=''
+
+DISTRO="$(uname -a)"
+
+case "$DISTRO" in
+    Darwin*)
+        DISABLE_NEW_ARGS='--old-args'
+        ;;
+    *)
+        ;;
+esac
+
 # Recursive, aggregate progress
-alias rsync='rsync ${DISABLE_IPV6} --old-args -a --info progress2'
+alias rsync='rsync ${DISABLE_IPV6} ${DISABLE_NEW_ARGS} -a --info progress2'
