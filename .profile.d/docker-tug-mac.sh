4dbfa6597d7e54ea0d608890c119644c3e4bafcc
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2023-06-27 14:42:43 -0500

    refine env

--- .profile.d/docker-tug-mac.sh
+++ .profile.d/docker-tug-mac.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Work around Docker quirks and cross-compiler toolchain gaps.
-# Note that this may break minikube.
+# Note that this may break minikube, cross, crit, etc.
 export DOCKER_DEFAULT_PLATFORM='linux/amd64'
