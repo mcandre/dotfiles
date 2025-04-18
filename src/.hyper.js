8bedee536ca2819d3b67e52516d95ec23bd7e1c2
Andrew <githubmcandre.gumdrop164@passfwd.com>
2025-04-18 09:53:12 -0500

    disable hyper-opacity

--- src/.hyper.js
+++ src/.hyper.js
@@ -151,7 +151,7 @@ module.exports = {
     //   `@company/project`
     //   `project#1.0.1`
     plugins: [
-        "hyper-opacity"
+        // "hyper-opacity"
     ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
