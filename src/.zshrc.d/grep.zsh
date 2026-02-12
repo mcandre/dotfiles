473e205cf7701c1bf190f698bc0845e4e16a1b43
Andrew Pennebaker <n4jm4@pm.me>
2026-02-11 18:37:23 -0600

    optimize interactive grep output

--- src/.zshrc.d/grep.zsh
+++ src/.zshrc.d/grep.zsh
@@ -1,2 +1,4 @@
-# https://monokai.pro/zsh
-export GREP_OPTIONS='--color=auto'
+# Silently ignore binaries and enable colors
+export GREP_OPTIONS='-I --color=auto'
+# Render matching text in bold red
+export GREP_COLORS='ms=01;31'
