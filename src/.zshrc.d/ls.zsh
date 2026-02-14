5a707bcc8f6750525675c7c5570bab319f6eccaf
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2026-02-13 23:30:25 -0600

    drop dead code

--- src/.zshrc.d/ls.zsh
+++ src/.zshrc.d/ls.zsh
@@ -1,10 +1,2 @@
-if [ -f "$HOME/.dircolors" ]; then
-    eval "$(dircolors -b "$HOME/.dircolors")"
-fi
-
 # Enable file mode color coding where available
 alias ls='ls --color=auto'
-
-# https://monokai.pro/zsh
-export CLICOLOR=1
-export LSCOLORS=gxfxexdxcxegedabagacad
