7997be40e3dd51b0a19defb99d8ac67ac48db50c
Andrew Pennebaker <n4jm4@pm.me>
2026-02-17 02:37:50 -0600

    up

--- src/.zshrc.d/ripgrep.zsh
+++ src/.zshrc.d/ripgrep.zsh
@@ -1,2 +1,2 @@
 export RIPGREP_CONFIG_PATH="$HOME/.ripgreprc"
-alias rg='rg --colors "match:fg:red" --colors "path:fg:cyan" --colors "line:fg:white"'
+alias rg='rg --pretty --colors "match:fg:red" --colors "path:fg:cyan" --colors "line:fg:white"'
