b01cd163526a01523eec4f2adba5df4a4084e6e1
Andrew Pennebaker <n4jm4@pm.me>
2026-04-03 16:24:40 -0500

    up

--- src/.zshenv.d/zcache.zsh
+++ src/.zshenv.d/zcache.zsh
@@ -2,7 +2,6 @@
 zcache() {
     zcompile ~/.zshenv
     zcompile ~/.zshrc
-
     [ -d ~/.zshenv.d ] && for f in $(find ~/.zshenv.d -name '*.zsh' -print); do zcompile "$f"; done
     [ -d ~/.zshrc.d ] && for f in $(find ~/.zshrc.d -name '*.zsh' -print); do zcompile "$f"; done
 }
